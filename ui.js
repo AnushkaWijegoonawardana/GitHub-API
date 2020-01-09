f6ccd140bdb17f3887ed7e4c85e75e8e0f2809d4
anushka wijegoonawrdana <anushkaduwolka123@gmail.com>
2020-01-09 06:42:16 +0530

    The Undefined Error is fixed

--- ui.js
+++ ui.js
@@ -63,7 +63,7 @@ class UI {
   showRepos(repos) {
     let repooutput = "";
 
-    repos.forEach(repo => {
+    repos.forEach(function(repo) {
       repooutput += `<div class="card card-body mb-2">
       <div class="row">
         <div class="col-md-8">
